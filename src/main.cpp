b00f3ad10668a38bf05a4165842a2deebb64bbd5
Jakub Kettner <140708360+dulakgg@users.noreply.github.com>
2025-08-24 12:24:07 +0200

    Modify music playback logic in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,22 +5,23 @@
 using namespace geode::prelude;
 
 class $modify(MyGJBaseGameLayer, GJBaseGameLayer) {
-	struct Fields {
-	    bool playing = false;
-	};
-
+	bool init() {
+		if (!GJBaseGameLayer::init()) return false;
+		channel =  FMODAudioEngine::sharedEngine()->getNextChannelId();
+		return true;
+	}
 	void destroyPlayer(PlayerObject* p0, GameObject* p1) {
 		int howManyDeaths = Mod::get()->getSettingValue<int64_t>("how-many-deaths");
 		int currentAttempts = this->m_attempts.get();
 		if (currentAttempts == howManyDeaths && !playing) {
 			playing = true;
-			FMODAudioEngine::sharedEngine()->playMusic("Sisyphus.mp3", true, 0.0f, -1);
+			FMODAudioEngine::sharedEngine()->playMusic("Sisyphus.mp3", true, 0.0f, channel);
 		}
 		GJaseGameLayer::destroyPlayer(p0, p1);
 	}
 	void checkpointActivated(CheckpointGameObject* p0) {
 		if (playing) {
-			FMODAudioEngine::sharedEngine()->pauseMusic(-1);
+			FMODAudioEngine::sharedEngine()->pauseMusic(channel);
 			playing == false;
 		}
 		GJaseGameLayer::checkpointActivated(p0);
