9c3e48a938bca90bd87933ca469b83b60cdf5b25
Jakub Kettner <140708360+dulakgg@users.noreply.github.com>
2025-08-24 12:27:25 +0200

    Fix channel ID retrieval and player destruction logic

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,10 @@
 using namespace geode::prelude;
 
 class $modify(MyGJBaseGameLayer, GJBaseGameLayer) {
+public:
 	bool init() {
 		if (!GJBaseGameLayer::init()) return false;
-		channel =  FMODAudioEngine::sharedEngine()->getNextChannelId();
+		channel =  FMODAudioEngine::sharedEngine()->getNextChannelID();
 		return true;
 	}
 	void destroyPlayer(PlayerObject* p0, GameObject* p1) {
@@ -17,12 +18,12 @@ class $modify(MyGJBaseGameLayer, GJBaseGameLayer) {
 			playing = true;
 			FMODAudioEngine::sharedEngine()->playMusic("Sisyphus.mp3", true, 0.0f, channel);
 		}
-		GJaseGameLayer::destroyPlayer(p0, p1);
+		GJBaseGameLayer::destroyPlayer(p0, p1);
 	}
 	void checkpointActivated(CheckpointGameObject* p0) {
 		if (playing) {
 			FMODAudioEngine::sharedEngine()->pauseMusic(channel);
-			playing == false;
+			playing = false;
 		}
 		GJaseGameLayer::checkpointActivated(p0);
 	}
