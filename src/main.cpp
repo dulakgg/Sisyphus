a2476f2e5bd881e05557b46b88d112b8e357e4b0
Jakub Kettner <140708360+dulakgg@users.noreply.github.com>
2025-08-25 12:47:47 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,6 @@
 
 using namespace geode::prelude;
 struct MusicState   {
-    static FMOD::System* system;
-    static FMOD::Sound* sound;
-    static FMOD::Sound* sound2;
-    static FMOD::Sound* sound3;
-    static FMOD::Sound* sound4;
-    static FMOD::Channel* channel;
     static bool playing;
     static bool isPlatformer;
     static float idleTimer;
@@ -21,19 +15,15 @@ struct MusicState   {
     static bool fadingImage;
     static CCSprite* overlaySprite;
     static float howLongIdling;
+    static int effectId;
 };
 
+int MusicState::effectId = -1;
 float MusicState::howLongIdling = Mod::get()->getSettingValue<float>("time-idling");
 float MusicState::howLongToFade = Mod::get()->getSettingValue<float>("time-to-fade");
 float MusicState::idleTimer = 0;
 bool MusicState::fadingImage = false;
 CCSprite* MusicState::overlaySprite = nullptr;
-FMOD::System* MusicState::system = nullptr;
-FMOD::Sound* MusicState::sound = nullptr;
-FMOD::Sound* MusicState::sound2 = nullptr;
-FMOD::Sound* MusicState::sound3 = nullptr;
-FMOD::Sound* MusicState::sound4 = nullptr;
-FMOD::Channel* MusicState::channel = nullptr;
 bool MusicState::isPlatformer = false;
 bool MusicState::playing = false;
 
@@ -46,108 +36,33 @@ class $modify(MyGJBaseGameLayer, GJBaseGameLayer) {
         else  {
             MusicState::isPlatformer = false;
         }
-        if (!MusicState::system) {
-            MusicState::system = FMODAudioEngine::sharedEngine()->m_system;
-        }
-        if (MusicState::system && !MusicState::sound) {
-            MusicState::system->createSound("Sisyphus.mp3", FMOD_DEFAULT, nullptr, & MusicState::sound);
-            MusicState::system->createSound("Sisyphus2.mp3", FMOD_DEFAULT, nullptr, & MusicState::sound2);
-            MusicState::system->createSound("Sisyphus3.mp3", FMOD_DEFAULT, nullptr, & MusicState::sound3);
-            MusicState::system->createSound("Sisyphus4.mp3", FMOD_DEFAULT, nullptr, & MusicState::sound4);
-        }
         return true;
     }
 
     void checkpointActivated(CheckpointGameObject* p0) {
         if (MusicState::playing) {
-            if (MusicState::channel) {
-                MusicState::channel->setPosition(0, FMOD_TIMEUNIT_MS);
-                MusicState::channel->setPaused(true);
-            }
+            FMODAudioEngine::get()->stopEffect(effectId);
             MusicState::playing = false;
         }
         GJBaseGameLayer::checkpointActivated(p0);
-    }
-
-    void onExit() {
-        if (MusicState::channel) {
-            MusicState::channel->stop();
-            MusicState::channel = nullptr;
-        }
-
-        if (MusicState::sound) {
-            MusicState::sound->release();
-            MusicState::sound = nullptr;
-        }
-
-        if (MusicState::sound2) {
-            MusicState::sound2->release();
-            MusicState::sound2 = nullptr;
-        }
-
-        if (MusicState::sound3) {
-            MusicState::sound3->release();
-            MusicState::sound3 = nullptr;
-        }
-
-        if (MusicState::sound4) {
-            MusicState::sound4->release();
-            MusicState::sound4 = nullptr;
-        }
-        MusicState::playing = false;
-        GJBaseGameLayer::onExit();
     }
 };
 
 class $modify(MyPlayLayer, PlayLayer) {
     void togglePracticeMode(bool practiceMode) {
-        if (!MusicState::system) {
-            PlayLayer::togglePracticeMode(practiceMode);
-            return;
-        }
         if (practiceMode) {
-            if (MusicState::channel) {
-                MusicState::channel->stop();
-                MusicState::channel = nullptr;
-                MusicState::playing = false;
+            if (MusicState::playing) {
+                FMODAudioEngine::get()->stopEffect(effectId);
+                effectId = FMODAudioEngine::get()->playEffect("Sisyphus2.mp3");
             }
-            if (MusicState::sound2) {
-                MusicState::system->playSound(MusicState::sound2, nullptr, true, &MusicState::channel);
-                if (MusicState::channel) {
-                    MusicState::channel->setPaused(false);
-                    MusicState::playing = true;
-                }
-            }
-        }
-        else  {
-            if (MusicState::channel) {
-                MusicState::channel->stop();
-                MusicState::channel = nullptr;
-            }
-            MusicState::playing = false;
         }
         PlayLayer::togglePracticeMode(practiceMode);
     }
 
     void fullReset() {
-        if (MusicState::channel) {
-            MusicState::channel->stop();
-            MusicState::channel = nullptr;
-            MusicState::playing = false;
-        }
-        if (MusicState::sound2) {
-            MusicState::system->playSound(MusicState::sound2, nullptr, true, &MusicState::channel);
-            if (MusicState::channel) {
-                MusicState::channel->setPaused(false);
-                MusicState::playing = true;
-            }
-        }
-        else  {
-            if (MusicState::channel) {
-                MusicState::channel->stop();
-                MusicState::channel = nullptr;
-            }
-            MusicState::playing = false;
+        if (MusicState::playing) {
+            FMODAudioEngine::get()->stopEffect(effectId);
+            effectId = FMODAudioEngine::get()->playEffect("Sisyphus2.mp3");
         }
         PlayLayer::fullReset();
     }
@@ -156,13 +71,8 @@ class $modify(MyPlayLayer, PlayLayer) {
         int howManyDeaths = Mod::get()->getSettingValue<int64_t>("how-many-deaths");
         int currentAttempts = this->m_attempts;
         if (currentAttempts > 0 && howManyDeaths > 0 && currentAttempts % howManyDeaths == 0 && !MusicState::playing && MusicState::isPlatformer) {
-            if (MusicState::system && MusicState::sound) {
-                MusicState::system->playSound(MusicState::sound, nullptr, true, &MusicState::channel);
-                if (MusicState::channel) {
-                    MusicState::channel->setPaused(false);
-                    MusicState::playing = true;
-                }
-            }
+            effectId = FMODAudioEngine::get()->playEffect("Sisyphus.mp3");
+            MusicState::playing = true;
         }
         PlayLayer::destroyPlayer(p0, p1);
     }
